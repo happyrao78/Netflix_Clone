2e6a17acf9b41f75b2fad00f54573ad341a9a70f
HAPPY YADAV <138770813+happyrao78@users.noreply.github.com>
2024-01-09 23:25:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var typed = new Typed('#element', {
+            strings: ['Hey, myself Happy Yadav', 'You gotta get it to get it', 'There’s no business like blow business'],
+            typeSpeed: 100,
+        });
+    
